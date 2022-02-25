1f1567392aa35e0605bd25a4637ef0462649a5ed
Pantelija <pantelijastosic@gmail.com>
2022-02-25 04:42:51 +0100

    Commit sredjena pretraga

---
+++ app/Http/Resources/UgovorResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UgovorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'user' => $this->getUser,
+            'tip_ugovora' => $this->getTipUgovora,
+            'tip_servisa' => $this->getTipServisa,
+            'naziv_servisa' => $this->getNazivServisa,
+            'lokacija_app' => $this->getLokacijaApp,
+            'connectivity_plan' => $this->connectivity_plan,
+            'ip_adresa' => $this->ip_adresa
+        ];
+    }
+}
